2f5375b106e97b45ae4d50ed5720beee3b9346c5
Maria Luiza <maluzinhex@gmail.com>
2025-10-01 16:41:55 -0300

    Ativ13

---
+++ src/main/java/Atv13/exercicio_02.java
@@ -0,0 +1,16 @@
+package Atv13;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio_02 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informa a quantidade de unidades no estoque: ");
+        int estoque = sc.nextInt();
+        String info = (estoque < 20) ? "Reabastecer imediatamente" : "Estoque insuficiente";
+        System.out.println("Quantidade no estoque: " + info);
+        sc.close();
+    }
+}
---
+++ src/main/java/Atv13/exercicio_07.java
@@ -0,0 +1,16 @@
+package Atv13;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio_07 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual a quantidade de defeitos do lote ? ");
+        int sensor = sc.nextInt();
+        String info = (sensor > 5) ? "Lote reprovado" : "Lote aprovado";
+        System.out.println("" + info);
+        sc.close();
+    }
+}
