397cd4d7f610437a9f420f942c58c2053438a010
Acer <guilhermefelberque.2009@gmail.com>
2025-10-08 22:42:58 -0300

    Ativida Senai

---
+++ src/main/java/Atv15_Lista_While/exercicio_05.java
@@ -0,0 +1,18 @@
+package Atv15_Lista_While;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio_05 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int pressao = 0;
+
+        while (pressao < 120) {
+            pressao++;
+            System.out.println("A pressão atingida foi: " + pressao);
+        }
+        sc.close();
+    }
+}
